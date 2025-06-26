349d9f2ec3a04176dd47c0a7e4685da898fb6b91
eightex18 <hello.eightex@gmail.com>
2025-06-26 18:28:55 +0530

    added cors to the strapi configuration

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -2,7 +2,21 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        '*',
+        'http://localhost:3000',
+        'http://localhost:1337',
+        'https://trex-next.vercel.app',
+        'https://graceful-chocolate-74cf3f5835.strapiapp.com'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+    }
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
