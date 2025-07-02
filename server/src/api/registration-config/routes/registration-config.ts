525f5d15b7ff47ec7f2e7d0e8b87853d7093b80e
eightex18 <hello.eightex@gmail.com>
2025-07-03 00:31:24 +0530

    CORS error try 2

--- server/src/api/registration-config/routes/registration-config.ts
+++ server/src/api/registration-config/routes/registration-config.ts
@@ -15,20 +15,5 @@ export default {
         policies: [],
       },
     },
-    {
-      method: 'OPTIONS',
-      path: '/registration-config',
-      handler: (ctx) => {
-        ctx.set('Access-Control-Allow-Origin', '*');
-        ctx.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        ctx.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, Accept, X-Requested-With');
-        ctx.status = 200;
-        ctx.body = '';
-      },
-      config: {
-        auth: false,
-        policies: [],
-      },
-    },
   ],
 }; 
